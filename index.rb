266fc1196dfb12b6d45900d6121035585bcdcec5
NagaRaju <47264640+nagarajub22@users.noreply.github.com>
2024-03-12 17:32:49 +0530

    Update index.rb

--- index.rb
+++ index.rb
@@ -1,1 +1,12 @@
-print "Hello world\n";
+require 'socket'
+
+server = TCPServer.new(3000)
+
+loop do
+  client = server.accept
+  request = client.gets
+
+  response = "HTTP/1.1 200 OK\r\nContent-Type:text/html\r\n\r\nHello, World!"
+  client.puts response
+  client.close
+end
